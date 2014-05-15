78b4b3a8f147208f88bb8df69c4f77db77509ab4
Ben Keen <ben.keen@gmail.com>
2014-05-14 23:01:19 -0700

    1.0.4

---
+++ database_integrity.php
@@ -0,0 +1,20 @@
+<?php
+
+$STRUCTURE = array();
+$STRUCTURE["tables"] = array();
+$STRUCTURE["tables"]["module_swift_mailer_email_template_fields"] = array(
+  array(
+    "Field"   => "email_template_id",
+    "Type"    => "mediumint(9)",
+    "Null"    => "NO",
+    "Key"     => "PRI",
+    "Default" => ""
+  ),
+  array(
+    "Field"   => "return_path",
+    "Type"    => "varchar(255)",
+    "Null"    => "NO",
+    "Key"     => "",
+    "Default" => ""
+  )
+);
--- module.php
+++ module.php
@@ -7,8 +7,8 @@
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://modules.formtools.org";
-$MODULE["version"]         = "1.0.3";
-$MODULE["date"]            = "2010-09-11";
+$MODULE["version"]         = "1.0.4";
+$MODULE["date"]            = "2010-10-02";
 $MODULE["origin_language"] = "en_us";
 $MODULE["supports_ft_versions"] = "2.0.3";
 
