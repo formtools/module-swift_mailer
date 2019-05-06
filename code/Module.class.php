3414cdce739fffdffc55ccd413baf7ce9014a0c1
Ben Keen <ben.keen@gmail.com>
2019-05-05 18:27:18 -0700

    2.0.7

--- code/Module.class.php
+++ code/Module.class.php
@@ -18,8 +18,8 @@ class Module extends FormToolsModule
     protected $author = "Ben Keen";
     protected $authorEmail = "ben.keen@gmail.com";
     protected $authorLink = "https://formtools.org";
-    protected $version = "2.0.6";
-    protected $date = "2018-04-17";
+    protected $version = "2.0.7";
+    protected $date = "2019-05-05";
     protected $originLanguage = "en_us";
 
     protected $nav = array(
